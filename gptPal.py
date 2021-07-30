f59b2a11e994b65246bf782882a865a757d39d14
Jim Weinert <jweinert@digitalocean.com>
2021-07-30 12:52:23 -0500

    Revert "Fix systemd logs not catching print statements"
    This reverts commit 0996ec19254a7056ab6465e8296d01cf155e831f.

--- gptPal.py
+++ gptPal.py
@@ -1,24 +1,15 @@
 import os
 import json
-import logging
 import redis
 from ml import MLHandler
 from slack_sdk import WebClient
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from systemd.journal import JournaldLogHandler
 
 channel_ID = "C029UL7GFR7"
 bot_user_ID = "U029MFZNYBW"
 client = WebClient(token=os.environ['SLACK_API_TOKEN'])
 r = redis.Redis(host='localhost', port=6379, db=0)
 event_ids_key = "events"
-
-logger = logging.getLogger(__name__)
-journald_handler = JournaldLogHandler()
-
-journald_handler.setFormatter(logging.Formatter(
-    '[%(levelname)s] %(message)s'
-))
 
 # Sends the given message text in the given channel
 def sendMessageInChannel(channel, message):
@@ -55,18 +46,18 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write(bytes(challenge, "utf8"))
         elif "event" in body_as_json:
             event = body_as_json["event"]
-            logger.info(event["type"])
+            print(event["type"])
 
             added_cnt = r.sadd(event_ids_key, body_as_json["event_id"])
             if added_cnt == 0:
-                logger.info("event already exists")
+                print("event already exists")
             elif event["type"] == "app_mention":
-                logger.info("handling @mention")
+                print("handling @mention")
                 val = MLHandler(event["text"])
-                logger.info("Reponding with ", val)
+                print("Reponding with ", val)
                 mentionHandler(channel_ID, val, event["user"])
         else:
-            logger.info("no match")
+            print("no match")
 
 
 
