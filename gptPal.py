058dad42ddff8ea57c2e93f420b634beb7a342c9
Jim Weinert <jweinert@digitalocean.com>
2021-07-29 21:36:49 -0500

    Use redis to track processed events
    Slack will send duplicate requests for the same event. Use redis to track
    which events have been processed by gptPal.

--- gptPal.py
+++ gptPal.py
@@ -1,11 +1,14 @@
 import os
 import json
+import redis
 from slack_sdk import WebClient
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 channel_ID = "C029UL7GFR7"
 bot_user_ID = "U029MFZNYBW"
 client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+r = redis.Redis(host='localhost', port=6379, db=0)
+event_ids_key = "events"
 
 # Sends the given message text in the given channel
 def sendMessageInChannel(channel, message):
@@ -33,6 +36,7 @@ class handler(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('Content-Length', 0))
         post_body = self.rfile.read(content_len)
         body_as_json = json.loads(post_body.decode("utf-8"))
+
         if "challenge" in body_as_json:
             challenge = (body_as_json["challenge"])
             self.send_response(200)
@@ -42,7 +46,11 @@ class handler(BaseHTTPRequestHandler):
         elif "event" in body_as_json:
             event = body_as_json["event"]
             print(event["type"])
-            if event["type"] == "app_mention":
+
+            set_added_cnt = r.sadd(event_ids_key, body_as_json["event_id"])
+            if set_added_cnt == 0:
+                print("event already exists")
+            elif event["type"] == "app_mention":
                 print("handling @mention")
                 mentionHandler(channel_ID, event["text"], event["user"])
         else:
